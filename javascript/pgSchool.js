ee7f2a23911843377aef6e35ceb7d095b5abd524
ajamu1144 <ajamuemmanuel0@gmail.com>
2025-07-14 16:00:53 +0100

    i want to sleep 😪😪

--- javascript/pgSchool.js
+++ javascript/pgSchool.js
@@ -1,63 +1,104 @@
-let subjects = document.querySelectorAll('.subject')
+let subjects = document.querySelectorAll('.subject') // subjects in the unadded
 let add = document.getElementById('addSubject')
 let remove = document.getElementById('removeSubject')
 let added = document.getElementById('Added')
 let unadded = document.getElementById('unAdded')
 let selectedSub = ""
 let selected = false;
+let selectedButton = null;
+let addedSubjects;
 
-subjects.forEach(sub => {
-    sub.addEventListener('click', () => {
-        if(sub.classList.contains('subject') && !selected) {
-            document.querySelectorAll('.subject').forEach(el => {
-                el.style.background = ""
-            })
-            selected = true
-            sub.style.background = "red"
-            selectedSub = sub
-            add.style.display = "flex"
-            add.textContent = "Click to Add"
-        }
-        else{
-            sub.style.background = "red"
-            add.style.display = "none"
-            remove.style.display = "flex"
-        }
-        remove.addEventListener('click', () => {
-            sub.classList.add('subject')
-            // added.removeChild(sub)
-            unadded.insertBefore(sub, unadded.firstChild)
-            remove.style.display = "none"
-            sub.style.background = ""
+function changeBg(){
+    subjects.forEach(subject =>{
+        subject.addEventListener('click', ()=>{
+            if(!selected){
+                subject.style.background = 'red'
+                selected = true
+                selectedButton = subject;
+                add.style.display = 'block'
+            }
+            else{
+                selectedButton.style.background = 'white'
+                selectedButton = null;
+                selected = false
+                add.style.display = 'none'
+            }
         })
     })
+}
+changeBg()
+
+add.addEventListener('click', ()=>{
+    selected = false
+    added.appendChild(selectedButton)
+    selectedButton.classList.add('addedSubjects')
+    selectedButton.classList.remove('subject')
+    addedSubjects = document.querySelectorAll('.addedSubjects')
+    // removeBtn()
 })
 
-add.addEventListener('click', () => {
-    selectedSub.style.background = "lime"
-    selectedSub.classList.add('ad')
-    selectedSub.classList.remove('subject')
-    add.style.display = "none"
-    console.log(selectedSub)
-    added.appendChild(selectedSub)
-    selectedSub = ""
-
-})
-
-added.childNodes.forEach(child =>{
-    child.addEventListener('click', () => {
-        child.classList.remove('subject')
-        remove.style.display = "flex"
-        add.style.display = "none !important"
-        add.addEventListener('click', () => {
-            selectedSub.style.background = "lime"
-            add.style.display = "none"
-            selectedSub.classList.add('ad')
-            selectedSub.classList.remove('subject')
-            console.log(selectedSub)
-            unadded.removeChild(selectedSub)
-            selectedSub = ""
+function removeBtn(){
+    addedSubjects.forEach(added =>{
+        added.addEventListener('click', ()=>{
+            added.style.background = 'red'
         })
-    });
-})
+    })
+}
 
+removeBtn()
+
+// subjects.forEach(sub => {
+//     sub.addEventListener('click', () => {
+//         if(sub.classList.contains('subject') && !selected) {
+//             document.querySelectorAll('.subject').forEach(el => {
+//                 el.style.background = ""
+//             })
+//             selected = true
+//             sub.style.background = "red"
+//             selectedSub = sub
+//             add.style.display = "flex"
+//             add.textContent = "Click to Add"
+//         }
+//         else{
+//             sub.style.background = "red"
+//             add.style.display = "none"
+//             remove.style.display = "flex"
+//         }
+//         remove.addEventListener('click', () => {
+//             sub.classList.add('subject')
+//             // added.removeChild(sub)
+//             unadded.insertBefore(sub, unadded.firstChild)
+//             remove.style.display = "none"
+//             sub.style.background = ""
+//         })
+//     })
+// })
+//
+// add.addEventListener('click', () => {
+//     selectedSub.style.background = "lime"
+//     selectedSub.classList.add('ad')
+//     selectedSub.classList.remove('subject')
+//     add.style.display = "none"
+//     console.log(selectedSub)
+//     added.appendChild(selectedSub)
+//     selectedSub = ""
+//
+// })
+//
+// added.childNodes.forEach(child =>{
+//     child.addEventListener('click', () => {
+//         child.classList.remove('subject')
+//         remove.style.display = "flex"
+//         add.style.display = "none !important"
+//         add.addEventListener('click', () => {
+//             selectedSub.style.background = "lime"
+//             add.style.display = "none"
+//             selectedSub.classList.add('ad')
+//             selectedSub.classList.remove('subject')
+//             console.log(selectedSub)
+//             unadded.removeChild(selectedSub)
+//             selectedSub = ""
+//         })
+//     });
+// })
+//
